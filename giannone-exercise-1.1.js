47082961a354a714599fe75767ef9a88a3e329ce
Andrew Giannone <drewnonne@gmail.com>
2019-11-28 14:53:34 -0600

    Create giannone-exercise-1.1.js

---
+++ giannone-exercise-1.1.js
@@ -0,0 +1,14 @@
+/*
+=======================================
+; Title: Assignment 1.1
+; Author: Andrew Giannone
+; Date: November 27 2019
+; Description This program should use JavaScript to print the value of 2 numbers multiplied to the console.
+;======================================
+*/
+
+var firstNumber = 23
+
+var secondNumber = 2
+
+console.log(firstNumber*secondNumber);
